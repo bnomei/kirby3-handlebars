6956c53f0d40c40e9941777675871664de2804a6
Bruno Meilick <bnomei@users.noreply.github.com>
2020-09-28 20:52:14 +0100

    for better php7.4 support enforce tmp dir and not memory

--- snippets/handlebars/render.php
+++ snippets/handlebars/render.php
@@ -1,5 +1,5 @@
 <?php
-    $renderer = LightnCandy\LightnCandy::prepare($precompiledTemplate);
+    $renderer = LightnCandy\LightnCandy::prepare($precompiledTemplate, sys_get_temp_dir());
     if ($renderer && is_callable($renderer)) {
         echo $renderer($data);
     }
