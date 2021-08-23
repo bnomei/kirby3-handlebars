9ab5de82acd98ba2abb339e4578db88e32f52f5e
Bruno Meilick <b@bnomei.com>
2021-08-23 14:19:20 +0100

    :bug: extension html collided with html() helper :arrow_up: upgraded dependencies

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,42 +1,41 @@
-<?php return array (
-  'root' => 
-  array (
-    'pretty_version' => '3.4.5',
-    'version' => '3.4.5.0',
-    'aliases' => 
-    array (
+<?php return array(
+    'root' => array(
+        'pretty_version' => '3.4.6',
+        'version' => '3.4.6.0',
+        'type' => 'kirby-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'bnomei/kirby3-handlebars',
+        'dev' => false,
     ),
-    'reference' => NULL,
-    'name' => 'bnomei/kirby3-handlebars',
-  ),
-  'versions' => 
-  array (
-    'bnomei/kirby3-handlebars' => 
-    array (
-      'pretty_version' => '3.4.5',
-      'version' => '3.4.5.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => NULL,
+    'versions' => array(
+        'bnomei/kirby3-handlebars' => array(
+            'pretty_version' => '3.4.6',
+            'version' => '3.4.6.0',
+            'type' => 'kirby-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+        'getkirby/composer-installer' => array(
+            'pretty_version' => '1.2.1',
+            'version' => '1.2.1.0',
+            'type' => 'composer-plugin',
+            'install_path' => __DIR__ . '/../getkirby/composer-installer',
+            'aliases' => array(),
+            'reference' => 'c98ece30bfba45be7ce457e1102d1b169d922f3d',
+            'dev_requirement' => false,
+        ),
+        'zordius/lightncandy' => array(
+            'pretty_version' => 'v1.2.2',
+            'version' => '1.2.2.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../zordius/lightncandy',
+            'aliases' => array(),
+            'reference' => '448cb2855d7301fa5380d8228255d46e9be92903',
+            'dev_requirement' => false,
+        ),
     ),
-    'getkirby/composer-installer' => 
-    array (
-      'pretty_version' => '1.2.1',
-      'version' => '1.2.1.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => 'c98ece30bfba45be7ce457e1102d1b169d922f3d',
-    ),
-    'zordius/lightncandy' => 
-    array (
-      'pretty_version' => 'v1.2.2',
-      'version' => '1.2.2.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => '448cb2855d7301fa5380d8228255d46e9be92903',
-    ),
-  ),
 );
